7c09f0cac542638a27ac8a39cb73cd56de0325ce
NoelOC <noeloconnell88@gmail.com>
2019-01-18 09:55:43 +0000

    Update dev dependencies, wrap fileHolder click in requestAnimationFrame

--- src/quill.imageUploader.js
+++ src/quill.imageUploader.js
@@ -1,76 +1,85 @@
-import "./blots/image";
+import './blots/image';
 
-import "./quill.imageUploader.css";
+import './quill.imageUploader.css';
 
 class ImageUploader {
-  constructor(quill, options) {
-    this.quill = quill;
-    this.options = options;
-    this.range = null;
+	constructor(quill, options) {
+		this.quill = quill;
+		this.options = options;
+		this.range = null;
 
-    if (typeof this.options.upload !== "function")
-      console.warn(
-        "[Missing config] upload function that returns a promise is required"
-      );
+		if (typeof this.options.upload !== 'function')
+			console.warn(
+				'[Missing config] upload function that returns a promise is required'
+			);
 
-    var toolbar = this.quill.getModule("toolbar");
-    toolbar.addHandler("image", this.selectLocalImage.bind(this));
-  }
+		var toolbar = this.quill.getModule('toolbar');
+		toolbar.addHandler('image', this.selectLocalImage.bind(this));
+	}
 
-  selectLocalImage() {
-    this.range = this.quill.getSelection();
-    this.fileHolder = document.createElement("input");
-    this.fileHolder.setAttribute("type", "file");
-    this.fileHolder.setAttribute("accept", "image/*");
+	selectLocalImage() {
+		this.range = this.quill.getSelection();
+		this.fileHolder = document.createElement('input');
+		this.fileHolder.setAttribute('type', 'file');
+		this.fileHolder.setAttribute('accept', 'image/*');
+		this.fileHolder.setAttribute('style', 'visibility:hidden');
 
-    this.fileHolder.onchange = this.fileChanged.bind(this);
+		this.fileHolder.onchange = this.fileChanged.bind(this);
 
-    this.fileHolder.click();
-  }
+		document.body.appendChild(this.fileHolder);
 
-  fileChanged() {
-    const file = this.fileHolder.files[0];
+		window.requestAnimationFrame(() => {
+			this.fileHolder.click();
 
-    const fileReader = new FileReader();
+			window.requestAnimationFrame(() => {
+				document.body.removeChild(this.fileHolder);
+			});
+		});
+	}
 
-    fileReader.addEventListener(
-      "load",
-      () => {
-        let base64ImageSrc = fileReader.result;
-        this.insertBase64Image(base64ImageSrc);
-      },
-      false
-    );
+	fileChanged() {
+		const file = this.fileHolder.files[0];
 
-    if (file) {
-      fileReader.readAsDataURL(file);
-    }
+		const fileReader = new FileReader();
 
-    this.options.upload(file).then(
-      imageUrl => {
-        this.insertToEditor(imageUrl);
-      },
-      error => {
-        console.warn(error.message);
-      }
-    );
-  }
+		fileReader.addEventListener(
+			'load',
+			() => {
+				let base64ImageSrc = fileReader.result;
+				this.insertBase64Image(base64ImageSrc);
+			},
+			false
+		);
 
-  insertBase64Image(url) {
-    const range = this.range;
-    this.quill.insertEmbed(range.index, "imageBlot", `${url}`);
-  }
+		if (file) {
+			fileReader.readAsDataURL(file);
+		}
 
-  insertToEditor(url) {
-    const range = this.range;
-    // Delete the placeholder image
-    this.quill.deleteText(range.index, 2);
-    // Insert the server saved image
-    this.quill.insertEmbed(range.index, "image", `${url}`);
+		this.options.upload(file).then(
+			imageUrl => {
+				this.insertToEditor(imageUrl);
+			},
+			error => {
+				console.warn(error.message);
+			}
+		);
+	}
 
-    range.index++;
-    this.quill.setSelection(range, "api");
-  }
+	insertBase64Image(url) {
+		const range = this.range;
+		this.quill.insertEmbed(range.index, 'imageBlot', `${url}`);
+	}
+
+	insertToEditor(url) {
+		const range = this.range;
+		// Delete the placeholder image
+		this.quill.deleteText(range.index, 2);
+		// Insert the server saved image
+		this.quill.insertEmbed(range.index, 'image', `${url}`);
+
+		range.index++;
+		this.quill.setSelection(range, 'api');
+	}
 }
 
 export default ImageUploader;
